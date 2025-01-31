15e7a48326359913cc083494690a184a6a97c4f3
pasinduSudesh <sapsdilshan@gmail.com>
2025-01-31 14:32:44 +0530

    Change font styles

--- components/SignInButton.js
+++ components/SignInButton.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 const SignInButton = ({onClick}) => (
-  <button className="bg-secondary-light border-2 border-secondary-light text-white font-kanit py-2 px-8 rounded-4xl hover:bg-secondary-dark hover:border-secondary-dark transition duration-500"
+  <button className="bg-secondary-light border-2 border-secondary-light text-white font-kanit py-2 px-8 rounded-4xl hover:bg-secondary-dark hover:border-secondary-dark transition duration-300"
   onClick={() => onClick()}>
     Sign In
   </button>
