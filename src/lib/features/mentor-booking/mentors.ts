c24c91d8e9aaffe94b6eea2c97feb0b551d33a78
pasinduSudesh <sapsdilshan@gmail.com>
2025-05-25 16:05:22 +0530

    Logs

--- src/lib/features/mentor-booking/mentors.ts
+++ src/lib/features/mentor-booking/mentors.ts
@@ -18,6 +18,9 @@ const initialState: MentorsState = {
 };
 
 export const fetchMentors = createAsyncThunk("register/getMentors", async () => {
+  // Replace with your actual API endpoint or logic to fetch mentors
+
+    console.log("fetching ENV in serside", process.env);
     const aaa =  await fetch("http://192.168.1.5:3000/mentors.json");
     console.log("result", aaa);
     return aaa.json();
